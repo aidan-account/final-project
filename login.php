a0ce80f9f4b6154fd476e6443a6d49ecf6df9170
aidan-account <aidan.stanard@greenwichschools.org>
2016-06-07 20:50:17 -0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,49 @@
+<?php
+
+$host = "localhost";
+$user = "Aidan";
+$pass = "sasha66";
+$db = "test";
+
+mysql_connect($host, $user, $pass);
+mysql_select_db($db);
+
+if(isset($_POST['username'])){}
+    $username = $_Post['username'];
+    $password = $_POST['password'];
+    $sql = "SELECT * FROM users WHERE username='".$username."'AND password='".$password."' LIMIT 1"
+    $res = mysql_query($sql);
+    if(mysql_num_rows($res) == 1){
+      echo"You have successfully logged in.";
+      exit();
+    }else{
+
+      echo "Invalid login information. Please return to the previous page.";
+      exit();
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+  
+  </head>
+  <body>
+  <form method="post" action="login.php">
+  Username: <input type="text" name="username"/><br /><br />
+  Password: <input type="password" name="password" /><br /><br />
+  <input type="submit" name="submit" value="Log in">
+  </form>
+    <php>
+        
+    </php>
+    <script>
+   
+    </script>
+  
+    
+
+
+  
+  </body>
+</html>
